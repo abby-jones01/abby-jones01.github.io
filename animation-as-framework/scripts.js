f77999506ba5635c5da8b51a2351d2e47730825f
abby-jones01 <abby_jones@taylor.edu>
2023-10-24 12:59:22 -0400

    scroll trigger enabled

--- animation-as-framework/scripts.js
+++ animation-as-framework/scripts.js
@@ -1,18 +1,67 @@
-import gsap from "gsap";
-
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".panel");
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500",
+gsap.timeline({
+  srcollTrigger:{
+    scrub:1,
+    trigger:
+
   }
-});
+})
+
+
+//Horizontal Scroll Galleries
+if (document.getElementById("portfolio")) {
+    const horizontalSections = gsap.utils.toArray('.horiz-gallery-wrapper')
+
+    horizontalSections.forEach(function (sec, i) {
+
+      const pinWrap = sec.querySelector(".horiz-gallery-strip");
+
+      let pinWrapWidth;
+      let horizontalScrollLength;
+      
+      function refresh() {
+        pinWrapWidth = pinWrap.scrollWidth;
+        horizontalScrollLength = pinWrapWidth - window.innerWidth;
+      }
+
+      // window.addEventListener("load", function () {
+        refresh();
+        // Pinning and horizontal scrolling
+        let scrollTween = gsap.to(pinWrap, {
+          scrollTrigger: {
+            scrub: true,
+            trigger: sec,
+            pin: sec,
+            start: "center center",
+            end: () => `+=${pinWrapWidth}`,
+            invalidateOnRefresh: true
+          },
+          x: () => -horizontalScrollLength,
+          ease: "none"
+        });
+      
+      pinWrap.querySelectorAll("[data-speed-x]").forEach((el, i) => {
+        let speed = parseFloat(el.getAttribute("data-speed-x"));
+        gsap.to(el, {
+          x: () => (1 - speed) * (window.innerWidth + el.offsetWidth),
+          ease: "none",
+          scrollTrigger: {
+            containerAnimation: scrollTween,
+            trigger: el,
+            onRefresh: self => {
+              let start = Math.max(0, self.start);
+              self.setPositions(start, start + (self.end - self.start) / Math.abs(speed)); // adjust for how long it'll stay in view
+              self.animation.progress(0);
+            },
+            scrub: true
+          }
+        });
+      });
+      
+
+        ScrollTrigger.addEventListener("refreshInit", refresh);
+      // });
+    });
+}
